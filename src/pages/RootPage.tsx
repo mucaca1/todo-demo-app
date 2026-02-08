0e73713b9c06eb5f20f8ad5c13f27dd26e6058d2
mucaca1 <matejkrc1@gmail.com>
2026-02-08 22:23:06 +0100

    Version test

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -3,31 +3,27 @@ import { MenuBar } from "../components/MenuBar";
 import { Suspense } from "react";
 import TodoPage from "./TodoPage";
 import { SettingsPage } from "./SettingsPage";
-import { useQueries } from "@evolu/react";
-import { activeTodos, settings } from "../evolu-db/evolu-query";
 import { QueryRows } from "@evolu/common";
 import { MemoryRouter } from 'react-router-dom';
 
 export function RootPage({rows}: {rows: QueryRows[]}) {
     return (
         <MemoryRouter initialEntries={['/']} initialIndex={0}>
-            <BrowserRouter>
-                <MenuBar />
-                <Suspense >
-                    <div style={{ maxWidth: "90%", padding: "2rem" }}>
-                        <Routes>
-                            <Route
-                                path="/"
-                                element={<TodoPage todoRows={rows[0]} />}
-                            />
-                            <Route
-                                path="/settings"
-                                element={<SettingsPage settingRows={rows[1]} />}
-                            />
-                        </Routes>
-                    </div>
-                </Suspense>
-            </BrowserRouter>
+            <MenuBar />
+            <Suspense >
+                <div style={{ maxWidth: "90%", padding: "2rem" }}>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<TodoPage todoRows={rows[0]} />}
+                        />
+                        <Route
+                            path="/settings"
+                            element={<SettingsPage settingRows={rows[1]} />}
+                        />
+                    </Routes>
+                </div>
+            </Suspense>
         </MemoryRouter>
     );
 }
