73f18ca87e4b7acb0625d2549a333b9086a5b68e
mucaca1 <matejkrc1@gmail.com>
2026-02-08 22:13:24 +0100

    Version update test

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -8,7 +8,7 @@ export function MenuBar() {
         <AppBar position="static">
             <Toolbar>
                 <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                    ToDo Demo App - v1.0.1
+                    ToDo Demo App - v1.0.2
                 </Typography>
 
                 <Box>
